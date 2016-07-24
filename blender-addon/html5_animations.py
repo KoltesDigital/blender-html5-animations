102c434b19206d966f8dd4f18ec2015a6ced6e2b
Jonathan Giroux <bloutiouf@gmail.com>
2016-07-24 14:28:29 +0200

    Support for system coordinate change

--- blender-addon/html5_animations.py
+++ blender-addon/html5_animations.py
@@ -17,9 +17,16 @@ bl_info = {
 import bpy
 import json
 
-from bpy.props import StringProperty, BoolProperty, EnumProperty
+from bpy.props import (
+    BoolProperty,
+    EnumProperty,
+    StringProperty,
+)
 from bpy.types import Operator
-from bpy_extras.io_utils import ExportHelper
+from bpy_extras.io_utils import (
+    ExportHelper,
+    orientation_helper_factory,
+)
 
 
 Easings = bpy.types.GRAPH_OT_easing_type.bl_rna.properties['type'].enum_items
@@ -27,7 +34,26 @@ Extrapolations = bpy.types.GRAPH_OT_extrapolation_type.bl_rna.properties['type']
 Interpolations = bpy.types.GRAPH_OT_interpolation_type.bl_rna.properties['type'].enum_items
 
 
-def export_json(time_coefficient):
+CoordinateSystemChangeDataPaths = {
+    'location',
+    'delta_location',
+    'rotation_axis',
+    'rotation_euler',
+    'delta_rotation_euler',
+    'rotation_quaternion',
+    'delta_rotation_quaternion',
+}
+
+CoordinateSystemChangeAxisIndex = {
+    'X': (0, 1),
+    '-X': (0, -1),
+    'Y': (1, 1),
+    '-Y': (1, -1),
+    'Z': (2, 1),
+    '-Z': (2, -1),
+}
+
+def export_json(time_coefficient, coordinate_system_mapping):
     actions_obj = {}
 
     for action in bpy.data.actions:
@@ -37,19 +63,24 @@ def export_json(time_coefficient):
                 fcurves_obj[fcurve.data_path] = []
             fcurves_arr = fcurves_obj[fcurve.data_path]
 
+            array_index = fcurve.array_index
+            value_coefficient = 1
+            if coordinate_system_mapping is not None:
+                array_index, value_coefficient = coordinate_system_mapping(array_index)
+
             # Fill missing array indexes
-            while len(fcurves_arr) <= fcurve.array_index:
+            while len(fcurves_arr) <= array_index:
                 fcurves_arr.append(0)
 
             keyframes_arr = []
             for keyframe_point in fcurve.keyframe_points:
                 keyframe_arr = [
                     keyframe_point.co[0] * time_coefficient,
-                    keyframe_point.co[1],
+                    keyframe_point.co[1] * value_coefficient,
                     keyframe_point.handle_left[0] * time_coefficient,
-                    keyframe_point.handle_left[1],
+                    keyframe_point.handle_left[1] * value_coefficient,
                     keyframe_point.handle_right[0] * time_coefficient,
-                    keyframe_point.handle_right[1],
+                    keyframe_point.handle_right[1] * value_coefficient,
                     Interpolations[keyframe_point.interpolation].value,
                 ]
 
@@ -70,7 +101,7 @@ def export_json(time_coefficient):
                 Extrapolations[fcurve.extrapolation].value,
             ]
 
-            fcurves_arr[fcurve.array_index] = fcurve_obj
+            fcurves_arr[array_index] = fcurve_obj
 
         markers_arr = []
         for marker in action.pose_markers:
@@ -92,7 +123,10 @@ def export_json(time_coefficient):
     return actions_obj
 
 
-class Export(Operator, ExportHelper):
+HTML5AnimationsOrientationHelper = orientation_helper_factory("HTML5AnimationsOrientationHelper", axis_forward='-Z', axis_up='Y')
+
+
+class Export(Operator, ExportHelper, HTML5AnimationsOrientationHelper):
     """Export animation curves as CSS, JS, or JSON."""
     bl_idname = "html5_animations.export"
     bl_label = "Export HTML5 Animations"
@@ -103,6 +137,12 @@ class Export(Operator, ExportHelper):
         default="*.css;*.js;*.json",
         options={'HIDDEN'},
         maxlen=255,
+    )
+
+    change_coordinate_system = BoolProperty(
+        name="Change coordinate system",
+        description="Affected data paths: location, rotation_axis, rotation_euler, rotation_quaternion, scale + delta_",
+        default=False,
     )
 
     format = EnumProperty(
@@ -148,6 +188,13 @@ class Export(Operator, ExportHelper):
     def draw(self, context):
         layout = self.layout
 
+        layout.prop(self, "change_coordinate_system")
+        if self.change_coordinate_system:
+            layout.prop(self, "axis_forward")
+            layout.prop(self, "axis_up")
+
+        layout.separator()
+
         layout.prop(self, 'format', expand=True)
 
         if self.format == 'CSS':
@@ -165,10 +212,31 @@ class Export(Operator, ExportHelper):
             layout.prop(self, 'js_json_pretty_formatting')
 
     def execute(self, context):
+        if not self.filepath:
+            raise Exception("filepath not set")
+
+        coordinate_system_mapping = None
+        if self.change_coordinate_system:
+            forward_index, forward_coefficient = CoordinateSystemChangeAxisIndex[self.axis_forward]
+            up_index, up_coefficient = CoordinateSystemChangeAxisIndex[self.axis_up]
+            if forward_index == up_index:
+                raise Exception("Wrong coordinate system change")
+            right_index = 3 - forward_index - up_index
+            right_coefficient = 1 if (forward_index < up_index) == (forward_coefficient == up_coefficient) else -1
+
+            def coordinate_system_mapping(array_index):
+                if array_index == 0:
+                    return (right_index, right_coefficient)
+                if array_index == 1:
+                    return (forward_index, forward_coefficient)
+                if array_index == 2:
+                    return (up_index, up_coefficient)
+                return (array_index, 1)
+
         time_coefficient = context.scene.render.fps_base / context.scene.render.fps
 
         if self.format == 'CSS':
-            self.report({'ERROR'}, "Not implemented")
+            raise Exception("Not implemented")
 
         else:
             if self.js_json_pretty_formatting:
@@ -178,7 +246,7 @@ class Export(Operator, ExportHelper):
                 indent = None
                 separators = (',', ':')
 
-            obj = export_json(time_coefficient)
+            obj = export_json(time_coefficient, coordinate_system_mapping)
 
             if self.format == 'JS':
                 with open(self.filepath, 'w', encoding='utf-8') as f:
